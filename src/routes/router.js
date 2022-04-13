2e15445c42fc076f9b93761c07445ba105479ab6
Alan Nascimento <alan.nascimento@mercadolivre.com>
2022-04-13 18:23:55 -0300

    feat: use lazy and suspense at routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { CircularProgress } from '@material-ui/core';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import { DashboardPage } from 'src/pages';
 import { routes } from '.';
+
+const Dashboard = lazy(() => import('src/pages/dashboard/dashboard'));
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path={routes.USERS} component={DashboardPage} />
-        <Route exact path={routes.TODOS} component={DashboardPage} />
-        <Route render={() => <Redirect to={routes.USERS} />} />
-      </Switch>
+      <Suspense fallback={<CircularProgress />}>
+        <Switch>
+          <Route exact path={routes.USERS} component={Dashboard} />
+          <Route exact path={routes.TODOS} component={Dashboard} />
+          <Route render={() => <Redirect to={routes.USERS} />} />
+        </Switch>
+      </Suspense>
     </BrowserRouter>
   );
 };
